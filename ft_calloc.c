ef01b9caa155b04f62a2d050cb369ca1ff37d03d
Lavon Mushroom <lmushroo@si-f3.kzn.21-school.ru>
2020-11-10 18:15:20 +0300

    2

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: lmushroo <lmushroo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 14:41:27 by lmushroo          #+#    #+#             */
-/*   Updated: 2020/11/10 14:57:06 by lmushroo         ###   ########.fr       */
+/*   Updated: 2020/11/10 18:14:48 by lmushroo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 void		*ft_calloc(size_t count, size_t size)
 {
+	char	*str;
 
+	str = (char *)malloc(count * size);
+	if (str == NULL)
+		return (NULL);
+	ft_bzero(str, (count * size);
+		return(str);
+}
+
+int			main(void)
+{
+	char t[] = "1234qwer";
+
+	printf("%s\n", calloc(5. 4));
+	return (0);
 }
