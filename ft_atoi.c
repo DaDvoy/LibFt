c0294803b9d9057b8bc22531ea202bf8e46c1991
Lavon Mushroom <lmushroo@et-m1.kzn.21-school.ru>
2020-11-13 23:34:47 +0300

    4

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: lmushroo <lmushroo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/31 17:15:03 by lmushroo          #+#    #+#             */
-/*   Updated: 2020/11/13 20:58:15 by lmushroo         ###   ########.fr       */
+/*   Updated: 2020/11/13 23:22:08 by lmushroo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ int		ft_atoi(char *str)
 	b = 1;
 	c = a;
 	while (str[a] == '\t' || str[a] == '\v' || str[a] == '\f' || str[a] == '\r'
-	|| str[a] == '\n' || str[a] == '\f' || str[a] == ' ')
+	|| str[a] == '\n' || str[a] == ' ')
 		a++;
-	while ((str[a] >= 9 && str[a] <= 13) || str[a] == '-')
+	if (str[a] == '+' || str[a] == '-')
 	{
 		if (str[a] == '-')
-		{
 			b = b * -1;
-		}
+		else
+			b = 1;
 		a++;
 	}
 	while (str[a] >= '0' && str[a] <= '9')
--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: lmushroo <lmushroo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 23:00:29 by lmushroo          #+#    #+#             */
-/*   Updated: 2020/11/11 23:32:03 by lmushroo         ###   ########.fr       */
+/*   Updated: 2020/11/13 22:24:57 by lmushroo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ char		*ft_strdup(const char *s1)
 	size_t	len;
 	char	*str;
 
-	len = ft_strlen(s1);
-	str = (char *)malloc((len + 1) * sizeof(char));
+	len = ft_strlen(s1) + 1;
+	str = (char *)malloc((len) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	ft_strlcpy(str, s1, len);
+	ft_memcpy(str, s1, len);
 	return (str);
 }
